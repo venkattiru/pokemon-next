7e8766680b0e3223f25e4810fd0b8c8bdf5e2911
venkattiru <venkat.tiruveedhi@gmail.com>
2023-07-29 12:23:07 +0530

    removed next-themes
    unused import

--- app/page.js
+++ app/page.js
@@ -1,10 +1,8 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import { ThemeProvider } from 'next-themes'
 
 export default function Home() {
   return (
-    <ThemeProvider attribute='class'>
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Image src='/pokeball-md.png' alt="pokemon logo" width={300} height={500}/>
       <button>SignIn</button>
@@ -16,6 +14,5 @@ export default function Home() {
           </button>
           </Link>
     </main>
-    </ThemeProvider>
   )
 }
